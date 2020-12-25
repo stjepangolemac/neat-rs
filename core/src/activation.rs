a159a45e300e36e1a8838db9bed2684b849d0d1e
Stjepan Golemac <stjepan@tray.io>
2020-12-25 21:26:08 +0100

    Add more activation functions

--- core/src/activation.rs
+++ core/src/activation.rs
@@ -7,14 +7,32 @@ pub enum ActivationKind {
     Tanh,
     Relu,
     Step,
+    Logistic,
+    Identity,
+    Softsign,
+    Sinusoid,
+    Gaussian,
+    BentIdentity,
+    Bipolar,
+    Inverse,
+    SELU,
 }
 
 impl Distribution<ActivationKind> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> ActivationKind {
-        match rng.gen_range(0, 3) {
+        match rng.gen_range(0, 12) {
             0 => ActivationKind::Tanh,
             1 => ActivationKind::Relu,
-            _ => ActivationKind::Step,
+            2 => ActivationKind::Step,
+            3 => ActivationKind::Logistic,
+            4 => ActivationKind::Identity,
+            5 => ActivationKind::Softsign,
+            6 => ActivationKind::Sinusoid,
+            7 => ActivationKind::Gaussian,
+            8 => ActivationKind::BentIdentity,
+            9 => ActivationKind::Bipolar,
+            10 => ActivationKind::SELU,
+            _ => ActivationKind::Inverse,
         }
     }
 }
@@ -36,6 +54,28 @@ pub fn activate(x: f64, kind: &ActivationKind) -> f64 {
                 0.
             }
         }
-        _ => x,
+        ActivationKind::Logistic => 1. / (1. + (-x).exp()),
+        ActivationKind::Identity => x,
+        ActivationKind::Softsign => x / (1. + x.abs()),
+        ActivationKind::Sinusoid => x.sin(),
+        ActivationKind::Gaussian => (-x.powi(2)).exp(),
+        ActivationKind::BentIdentity => (((x.powi(2) + 1.).sqrt() - 1.) / 2.) + x,
+        ActivationKind::Bipolar => {
+            if x > 0. {
+                1.
+            } else {
+                -1.
+            }
+        }
+        ActivationKind::Inverse => 1. - x,
+        ActivationKind::SELU => {
+            let alpha = 1.6732632423543772;
+            let scale = 1.05070098735548;
+
+            let fx = if x > 0. { x } else { alpha * x.exp() - alpha };
+
+            fx * scale
+        }
+        _ => panic!("Unknown activation function"),
     }
 }
